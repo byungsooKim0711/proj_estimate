de1bc18b4ae09261b0e171f1945cd0fb0f80602a
kimbs <vlakd0711@naver.com>
2019-04-29 10:42:39 +0900

    cors mapping 추가

--- src/main/java/com/espid/estimate/config/WebConfig.java
+++ src/main/java/com/espid/estimate/config/WebConfig.java
@@ -3,14 +3,28 @@ package com.espid.estimate.config;
 import com.espid.estimate.log.ControllerLogInterceptor;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer  {
+
+    private final long MAX_AGE_SECONDS = 3600;
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
         /* registry.addInterceptor(new ControllerLogInterceptor()); */
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
+            .allowedHeaders("*")
+            .maxAge(MAX_AGE_SECONDS);
     }
 }
